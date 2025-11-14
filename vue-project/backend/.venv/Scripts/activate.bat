cc37bea1b40ca8dc8e8db8a699c5a760f19f0e17
姜凯升 <jiangkaisheng0730@gmail.com>
2025-11-14 17:04:58 +0800

    chore: stop tracking backend venv

--- vue-project/backend/.venv/Scripts/activate.bat
+++ vue-project/backend/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\ShareCache\lyx\Thild\软件工程\小组\SH-Drug-Mgmt\vue-project\backend\.venv
+set VIRTUAL_ENV=C:\Users\86181\Desktop\软件工程\vue-project\backend\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -25,7 +25,6 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(.venv) 
 
 :END
 if defined _OLD_CODEPAGE (
