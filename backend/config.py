eeb5159258d8c51b3b77d73dfc29a4b7ffb8158f
co-iced16 <2661168939@qq.com>
2025-12-03 18:37:33 +0800

    设置数据库路径，使用华为云服务器上的数据库文件，并且保留旧配置

--- backend/config.py
+++ backend/config.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret')
+    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///data.db')
     SQLALCHEMY_DATABASE_URI = 'sqlite:////var/www/SH-Drug-Mgmt/backend/data.db'
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
